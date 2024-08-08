ebc553c1bf66b602999b85bdf2236d2d6138edcb
SRIDHARANITHARAN <username420sri@gmail.com>
2024-08-08 14:25:36 -0700

    connection single ip only

--- stms.py
+++ stms.py
@@ -2,6 +2,7 @@
 import socket
 from subprocess import PIPE ,STDOUT ,Popen
 import threading
+conn_count=[]
 class Math_thread_stdout(threading.Thread):
     def __init__(self,proc,conn):
         threading.Thread.__init__(self)
@@ -19,23 +20,26 @@ class Math_thread_stdin(threading.Thread):
         self.conn = conn
         self.add = add
     def run(self):
-        print("incoming connection {}_{}".format(self.add[0],self.add[1]))
-        self.conn.sendall("Simple lightWight Math server :2024 version:0.1 \n Made by Sridharanitharan.B \n".encode())
-        #APPLICATION LAYER (7)
-        bc_proc = Popen(['bc'],stdin = PIPE,stdout= PIPE,stderr = STDOUT)
-        start_ = Math_thread_stdout(bc_proc,self.conn)
-        start_.start()
-        while bc_proc.poll() is None:
-            try:#PRESENTATION LAYER(6)
-                data = self.conn.recv(1024)
-                if not data:
-                    break
-                data = data.decode().strip()
-                query =  data + '\n'
-                bc_proc.stdin.write(query.encode())
-                bc_proc.stdin.flush()
-            except:
-                pass
+        try:
+            print("incoming connection {}_{}".format(self.add[0],self.add[1]))
+            self.conn.sendall("Simple lightWight Math server :2024 version:0.1 \n Made by Sridharanitharan.B \n".encode())
+            #APPLICATION LAYER (7)
+            bc_proc = Popen(['bc'],stdin = PIPE,stdout= PIPE,stderr = STDOUT)
+            start_ = Math_thread_stdout(bc_proc,self.conn)
+            start_.start()
+            while bc_proc.poll() is None:
+                try:#PRESENTATION LAYER(6)
+                    data = self.conn.recv(1024)
+                    if not data:
+                        break
+                    data = data.decode().strip()
+                    query =  data + '\n'
+                    bc_proc.stdin.write(query.encode())
+                    bc_proc.stdin.flush()
+                except:
+                    pass
+        except:
+            pass
 HOST = ''
 PORT = 9090
 #SESSION LAYER(5)
@@ -46,6 +50,14 @@ server.listen()
 print("listing port is........{}",PORT)
 while True:
     conn , add = server.accept()
+    for address in conn_count:
+        if address in add[0]:
+            print("connection is alrady in the server")
+            conn.send("already your  ip has been live in the chat".encode())
+            conn.close()
+            break 
+    conn_count.append(add[0])    
+        
     start_ = Math_thread_stdin(conn,add)
     start_.start()
-server.closed
+server.closed()
